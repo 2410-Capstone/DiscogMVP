ff217a6cc81bb1c964b2e3b824837bf1b5ea8201
Andy Edwards <andyedwards@outlook.com>
2025-04-14 15:33:50 -0400

    create orderfunction

--- server/db/orders.js
+++ server/db/orders.js
@@ -1,32 +1,49 @@
-const client = require('./client');
+const client = require("./client");
 
 // Order Functions
- // ---TO DO--- //
-const createOrder = async (userId, shippingAddress) => {}
+// ---TO DO--- //
+const createOrder = async ({ user_id, shipping_address, order_status, tracking_number, total }) => {
+  try {
+    const SQL = /*sql*/ `
+      INSERT INTO orders (user_id, shipping_address, order_status, tracking_number, total)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `;
+    const {
+      rows: [order],
+    } = await client.query(SQL, [user_id, shipping_address, order_status, tracking_number, total]);
+    return order;
+  } catch (error) {
+    console.error("Error creating order:", error);
+    throw error;
+  }
+};
 
-const getOrderByUserId = async (orderId) => {}
+const getOrderByUserId = async (orderId) => {};
 
-const getOrderById = async (orderId) => {
-
-}
+const getOrderById = async (orderId) => {};
 
 const createOrderItem = async (orderId, productId, quantity) => {
   try {
-    const { rows: [orderItem] } = await client.query(/*sql*/ `
+    const {
+      rows: [orderItem],
+    } = await client.query(
+      /*sql*/ `
       INSERT INTO order_items (order_id, product_id, quantity)
       VALUES ($1, $2, $3)
       RETURNING *;
-      `, [orderId, productId, quantity]);
-      return orderItem;
+      `,
+      [orderId, productId, quantity]
+    );
+    return orderItem;
   } catch (error) {
     console.error("Error creating order item:", error);
   }
-}
-
+};
 
 module.exports = {
   createOrder,
   getOrderByUserId,
   getOrderById,
   createOrderItem,
-}
+};
