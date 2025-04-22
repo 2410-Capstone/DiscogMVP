8986dbebd48fa359dd05287b4f8ce0f9143bbf02
Charley Lea <charleylea@att.net>
2025-04-22 17:36:20 -0400

    adding mock data into checkout screen

--- client/src/pages/user/Checkout.jsx
+++ client/src/pages/user/Checkout.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+// mock data
+const data = [
+  { id: 1, cartId: 1, productId: 3711, quantity: 2 },
+  { id: 2, cartId: 1, productId: 3406, quantity: 3 },
+  { id: 3, cartId: 1, productId: 7313, quantity: 1 },
+];
+
 const Checkout = ({ user }) => {
   const style = { marginTop: "50px" };
   return (
