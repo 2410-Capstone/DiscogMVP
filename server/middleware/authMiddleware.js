83b7965b88d822ab0842cc0971e1c1b0b6cc2133
Andy Edwards <andyedwards@outlook.com>
2025-04-22 22:51:22 -0400

    needed to change to make json calls

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,15 +4,15 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).json;
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(403).json;
     console.log("Authenticated User:", user);
     req.user = {
       id: user.id,
       user_role: user.user_role,
-    };    
+    };
     next();
   });
 };
