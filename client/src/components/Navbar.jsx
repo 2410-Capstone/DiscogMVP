9f31ef63dbbbc7e4c8e2d6eb5e7e90c4d581e303
Andy Edwards <andyedwards@outlook.com>
2025-04-24 16:02:04 -0400

    changed order history path to the correct one

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,64 +33,79 @@ function Navbar({ isAuthenticated, setUser, setToken, onSearch, user }) {
       document.body.style.overflow = isSearchVisible ? "hidden" : "auto";
     }
   }, [isSearchVisible]);
-  
+
   return (
     <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <div className="nav-left">
-            <Link to="/" className="nav-logo">DiscogMVP</Link>
+      <nav className='navbar'>
+        <div className='nav-container'>
+          <div className='nav-left'>
+            <Link to='/' className='nav-logo'>
+              DiscogMVP
+            </Link>
           </div>
 
           {!isOnMainPage && (
-            <div className="nav-center">
-              <Link to="/home" className="nav-button">Home</Link>
+            <div className='nav-center'>
+              <Link to='/home' className='nav-button'>
+                Home
+              </Link>
             </div>
           )}
 
-<div className="nav-right">
-  <button className="nav-button" onClick={toggleSearch} aria-label="Toggle Search">
-    <Search size={20} />
-  </button>
+          <div className='nav-right'>
+            <button className='nav-button' onClick={toggleSearch} aria-label='Toggle Search'>
+              <Search size={20} />
+            </button>
 
-  <Link to="/cart" className="nav-button">
-    <img src={cartImg} alt="Cart" className="cart-icon" />
-  </Link>
+            <Link to='/cart' className='nav-button'>
+              <img src={cartImg} alt='Cart' className='cart-icon' />
+            </Link>
 
-  {isAuthenticated ? (
-    <div className="dropdown-profile">
-      <img
-        src={user?.profilePic || defaultProfilePic}
-        alt="Profile"
-        className="profile-pic"
-      />
-      <div className="profile-dropdown-content">
-        <Link to="/account" className="dropdown-link">Account</Link>
-        <Link to="/profile" className="dropdown-link">Profile</Link>
-        <Link to="/order-history" className="dropdown-link">Order History</Link>
-        <button onClick={handleLogout} className="dropdown-link logout-button">Logout</button>
-      </div>
-    </div>
-  ) : (
-    <>
-      <Link to="/login" className="nav-button">Login</Link>
-      <Link to="/register" className="nav-button">Register</Link>
-    </>
-  )}
-</div>
-
+            {isAuthenticated ? (
+              <div className='dropdown-profile'>
+                <img src={user?.profilePic || defaultProfilePic} alt='Profile' className='profile-pic' />
+                <div className='profile-dropdown-content'>
+                  <Link to='/account' className='dropdown-link'>
+                    Account
+                  </Link>
+                  <Link to='/profile' className='dropdown-link'>
+                    Profile
+                  </Link>
+                  <Link to='/account/orders' className='dropdown-link'>
+                    Order History
+                  </Link>
+                  <button onClick={handleLogout} className='dropdown-link logout-button'>
+                    Logout
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <>
+                <Link to='/login' className='nav-button'>
+                  Login
+                </Link>
+                <Link to='/register' className='nav-button'>
+                  Register
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </nav>
 
       {isSearchVisible && (
-  <div className="dropdown show" onMouseLeave={() => setSearchVisible(false)}>
-    <SearchBar onSearch={onSearch} />
-    <div className="dropdown-links">
-      <Link to="/previously-viewed" className="dropdown-link">Previously Viewed</Link>
-      <Link to="/product-search" className="dropdown-link">Product Search</Link>
-    </div>
-  </div>
-)}
+        <div className='dropdown show' onMouseLeave={() => setSearchVisible(false)}>
+          <SearchBar onSearch={onSearch} />
+          <div className='dropdown-links'>
+            <Link to='/previously-viewed' className='dropdown-link'>
+              Previously Viewed
+            </Link>
+            <Link to='/product-search' className='dropdown-link'>
+              Product Search
+            </Link>
+          </div>
+        </div>
+      )}
     </>
   );
 }
