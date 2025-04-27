9f774686c0af0c3adb461abb1fcb5fe1eb35ca7e
Andy Edwards <andyedwards@outlook.com>
2025-04-27 14:12:58 -0400

    took out not null for email and password for guest users

--- server/db/db.js
+++ server/db/db.js
@@ -43,12 +43,11 @@ const createTables = async () => {
     await pool.query(/*sql*/ `
     CREATE TABLE users (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE,
+        password VARCHAR(255),
+        name VARCHAR(255),
         address VARCHAR(255),
         user_role role NOT NULL DEFAULT 'customer',
-        guest_u
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
       );
