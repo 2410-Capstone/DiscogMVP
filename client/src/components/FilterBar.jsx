2a26952e796a160622f967ded2a8564b2457734e
Andy Edwards <andyedwards@outlook.com>
2025-04-30 11:16:42 -0400

    add filterbar As a component

---
+++ client/src/components/FilterBar.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from "react";
+// import "../styles/App.css";
+
+const FilterBar = ({ genres, genreFilter, onFilterClick, onAllClick }) => {
+  const filterBarRef = useRef(null);
+
+  const scrollGenres = (direction) => {
+    if (filterBarRef.current) {
+      filterBarRef.current.scrollBy({
+        left: direction === "left" ? -150 : 150,
+        behavior: "smooth",
+      });
+    }
+  };
+  return (
+    <div className='filterBarWrapper'>
+      <button className='scrollBtn' onClick={() => scrollGenres("left")}>
+        &lt;
+      </button>
+      <div className='filterBar' ref={filterBarRef}>
+        <button className={`filterButton ${genreFilter.length === 0 ? "active" : ""}`} onClick={onAllClick}>
+          All genres
+        </button>
+        {genres.map((genre) => (
+          <button
+            key={genre}
+            className={`filterButton ${genreFilter.includes(genre) ? "active" : ""}`}
+            onClick={() => onFilterClick(genre)}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <button className='scrollBtn' onClick={() => scrollGenres("right")}>
+        &gt;
+      </button>
+    </div>
+  );
+};
+
+export default FilterBar;
