b4f0a71101c2c8e2440648eeb7ea3e3a523a262e
Charley Lea <charleylea@att.net>
2025-05-02 17:56:45 -0400

    added in contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,13 @@ import React, { useEffect, useState, useRef } from "react";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -11,7 +17,6 @@ import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FullSearchResults from "./components/FullSearchResults.jsx";
-
 
 import Welcome from "./pages/Welcome";
 import ItemList from "./pages/Home";
@@ -43,6 +48,7 @@ import WishlistsPage from "./components/WishlistsPage";
 import WishlistShare from "./components/wishlistShare";
 import CreateWishlist from "./components/CreateWishlist";
 import PublicWishlistPage from "./pages/user/PublicWishlistPage";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
   const location = useLocation();
@@ -55,20 +61,21 @@ function App() {
   const [genreFilter, setGenreFilter] = useState([]);
   const [allItems, setAllItems] = useState([]);
 
-useEffect(() => {
-  const fetchAllItems = async () => {
-    try {
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/products?limit=999`);
-      const data = await res.json();
-      setAllItems(data.products || []);
-    } catch (err) {
-      console.error("Failed to load products for global search:", err);
-    }
-  };
+  useEffect(() => {
+    const fetchAllItems = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/products?limit=999`
+        );
+        const data = await res.json();
+        setAllItems(data.products || []);
+      } catch (err) {
+        console.error("Failed to load products for global search:", err);
+      }
+    };
 
-  fetchAllItems();
-}, []);
-
+    fetchAllItems();
+  }, []);
 
   // useEffect(() => {
   //   const storedToken = localStorage.getItem("token");
@@ -119,7 +126,11 @@ useEffect(() => {
       return () => clearTimeout(timer);
     }, []);
 
-    return shouldRedirect ? <Navigate to='/login' replace /> : <div>Redirecting...</div>;
+    return shouldRedirect ? (
+      <Navigate to="/login" replace />
+    ) : (
+      <div>Redirecting...</div>
+    );
   };
 
   const handleSearch = (query) => {
@@ -130,85 +141,126 @@ useEffect(() => {
   return (
     <>
       {!isAdminRoute && (
-    <Navbar
-    isAuthenticated={isAuthenticated}
-    user={user}
-    onSearch={handleSearch}
-    allItems={allItems}
-  />
-  
+        <Navbar
+          isAuthenticated={isAuthenticated}
+          user={user}
+          onSearch={handleSearch}
+          allItems={allItems}
+        />
       )}
 
-      <div className='page-content'>
+      <div className="page-content">
         <Routes>
-          <Route path='*' element={<NotFound />} />
-          <Route path='/' element={<Welcome />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Welcome />} />
           <Route
-            path='/home'
-            element={<ItemList searchTerm={searchTerm} genreFilter={genreFilter} setGenreFilter={setGenreFilter} />}
+            path="/home"
+            element={
+              <ItemList
+                searchTerm={searchTerm}
+                genreFilter={genreFilter}
+                setGenreFilter={setGenreFilter}
+              />
+            }
           />
-        <Route path="/search" element={<FullSearchResults allItems={allItems} />} />
+          <Route
+            path="/search"
+            element={<FullSearchResults allItems={allItems} />}
+          />
 
-
-
-          <Route path='/home/:productId' element={<ProductDetails />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route path="/home/:productId" element={<ProductDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
           {/* User routes */}
-          <Route path='/account' element={isAuthenticated ? <Account user={user} /> : <Navigate to='/login' />} />
           <Route
-            path='/profile/:username'
-            element={isAuthenticated ? <Profile user={user} /> : <Navigate to='/login' />}
+            path="/account"
+            element={
+              isAuthenticated ? (
+                <Account user={user} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
           />
           <Route
-            path='/account/orders'
-            element={isAuthenticated ? <UserOrders user={user} /> : <Navigate to='/login' />}
+            path="/profile/:username"
+            element={
+              isAuthenticated ? (
+                <Profile user={user} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
           />
           <Route
-            path='/account/manage-account'
-            element={isAuthenticated ? <ManageAccount user={user} /> : <Navigate to='/login' />}
+            path="/account/orders"
+            element={
+              isAuthenticated ? (
+                <UserOrders user={user} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/account/manage-account"
+            element={
+              isAuthenticated ? (
+                <ManageAccount user={user} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
           />
 
+          <Route
+            path="/wishlists"
+            element={
+              isAuthenticated ? <WishlistsPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/wishlists/new"
+            element={
+              isAuthenticated ? <CreateWishlist /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/wishlists/:id"
+            element={isAuthenticated ? <Wishlist /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/wishlists/share/:shareId"
+            element={<PublicWishlistPage />} // Public view doesn't require auth
+          />
 
-          <Route 
-            path="/wishlists" 
-            element={isAuthenticated ? <WishlistsPage /> : <Navigate to="/login" />} 
-          />
-          <Route 
-            path="/wishlists/new" 
-            element={isAuthenticated ? <CreateWishlist /> : <Navigate to="/login" />} 
-          />
-          <Route 
-            path="/wishlists/:id" 
-            element={isAuthenticated ? <Wishlist /> : <Navigate to="/login" />} 
-          />
-          <Route 
-            path="/wishlists/share/:shareId" 
-            element={<PublicWishlistPage />}  // Public view doesn't require auth
-          />
-
-          <Route path='/guest-order-lookup' element={<GuestOrderLookup />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/order-confirmation' element={<OrderConfirmation />} />
-
-          
+          <Route path="/guest-order-lookup" element={<GuestOrderLookup />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/order-confirmation" element={<OrderConfirmation />} />
+          <Route path="contact" element={<Contact />} />
 
           {/* Admin routes using AdminLayout + permissions */}
           <Route
-            path='/admin/*'
-            element={isAuthenticated && user?.user_role === "admin" ? <AdminLayout user={user} /> : <AdminRedirect />}
+            path="/admin/*"
+            element={
+              isAuthenticated && user?.user_role === "admin" ? (
+                <AdminLayout user={user} />
+              ) : (
+                <AdminRedirect />
+              )
+            }
           >
             <Route index element={<Dashboard />} />
 
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='users' element={<AdminUserList />} />
-            <Route path='users/:id/edit' element={<AdminEditUser />} />
-            <Route path='inventory' element={<Inventory />} />
-            <Route path='edit-product/:id' element={<EditProduct />} />
-            <Route path='products/new' element={<AddProduct />} />
-            <Route path='orders' element={<AdminOrders />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="users" element={<AdminUserList />} />
+            <Route path="users/:id/edit" element={<AdminEditUser />} />
+            <Route path="inventory" element={<Inventory />} />
+            <Route path="edit-product/:id" element={<EditProduct />} />
+            <Route path="products/new" element={<AddProduct />} />
+            <Route path="orders" element={<AdminOrders />} />
           </Route>
         </Routes>
       </div>
